8ad53f0bb11ce7fcb2411cf085cecf04f97972a5
vitoriahelen-git <vitoriahellen3004@gmail.com>
2020-09-19 20:59:23 -0300

    Resolução de exercicio

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace leet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto; 
+            Console.Write("Mensagem:");
+            texto = Console.ReadLine().ToUpper();
+            Console.Write("Voce digitou:" + texto.Replace("A","4").Replace("E","3").Replace("I","1").Replace("L","1").Replace("O","0").Replace("T","7").Replace("S","5")); 
+
+        }
+    }
+}
